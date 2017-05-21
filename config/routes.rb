0fa0a3c74c0f70197894a2e0b40bd736ae7c6199
reid sherman <reid@debian.com>
2017-05-21 14:14:09 -0400

    11.1

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   
   resources :users
+  resources :account_activations, only: [:edit]
   #get 'static_pages/home'
   #get 'static_pages/help'
   #get 'static_pages/about'
