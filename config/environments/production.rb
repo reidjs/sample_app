57e4f5f705f2b2ff591fd8b6309c9f9170a05b65
reid sherman <reid@debian.com>
2017-05-25 17:17:11 -0400

    changed email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,12 +9,12 @@ Rails.application.configure do
   host = 'fast-headland-92797.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :address        => 'coral.nixihost.com',
+    :port           => '465',
     :authentication => :plain,
-    :user_name      => ENV['app68315561@heroku.com'],
-    :password       => ENV['wlgcxuxi0876'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['noreply@reidsherman.com'],
+    :password       => ENV['beef869riot650'],
+    :domain         => 'reidsherman.com',
     :enable_starttls_auto => true
   }
   # Settings specified here will take precedence over those in config/application.rb.
